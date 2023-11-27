07fe8993745c40dfa125f19803e5d2e4056b5642
nataliacaixeta <natncaixeta@hotmail.com>
2023-11-27 11:28:29 -0300

    first commit

---
+++ js/dados.js
@@ -0,0 +1,518 @@
+var MENU = {
+    "bolos": [
+        {
+            "id": "bolo-amora",
+            "img": "./img/bolos/amora.png",
+            "name": "Bolo Amora",
+            "dsc": "Bolo Amora",
+            "price": 80
+        },
+        {
+            "id": "bolo-blueberry",
+            "img": "./img/bolos/blueberry.png",
+            "name": "Bolo Blueberry",
+            "dsc": "Bolo Blueberry",
+            "price": 80
+        },
+        {
+            "id": "bolo-brigadeiro",
+            "img": "./img/bolos/brigadeiro.png",
+            "name": "Bolo Brigadeiro",
+            "dsc": "Bolo Brigadeiro",
+            "price": 80
+        },
+        {
+            "id": "bolos-cenoura",
+            "img": "./img/bolos/cenoura.png",
+            "name": "Bolo Cenoura",
+            "dsc": "Bolo Cenoura",
+            "price": 80
+        },
+        {
+            "id": "bolos-ferrero-rocher",
+            "img": "./img/bolos/ferrero-rocher.png",
+            "name": "Bolo Ferrero Rocher",
+            "dsc": "Bolo Ferrero Rocher",
+            "price": 80
+        },
+        {
+            "id": "bolos-framboesa",
+            "img": "./img/bolos/framboesa.png",
+            "name": "Bolo Framboesa",
+            "dsc": "Bolo Framboesa",
+            "price": 80
+        },
+        {
+            "id": "bolos-laranja",
+            "img": "./img/bolos/laranja.png",
+            "name": "Bolo Laranja",
+            "dsc": "Bolo Laranja",
+            "price": 80
+        },
+        {
+            "id": "bolos-limao",
+            "img": "./img/bolos/limao.png",
+            "name": "Bolo Limao",
+            "dsc": "Bolo Limao",
+            "price": 60
+        },
+        {
+            "id": "bolos-mesclado",
+            "img": "./img/bolos/mesclado.png",
+            "name": "Bolo Mesclado",
+            "dsc": "Bolo Mesclado",
+            "price": 20
+        },
+        {
+            "id": "bolos-milho",
+            "img": "./img/bolos/milho.png",
+            "name": "Bolo Milho",
+            "dsc": "Bolo Milho",
+            "price": 20
+        },
+        {
+            "id": "bolos-morango",
+            "img": "./img/bolos/morango.png",
+            "name": "Bolo Morango",
+            "dsc": "Bolo Morango",
+            "price": 80
+        },
+        {
+            "id": "bolos-oreo-morango",
+            "img": "./img/bolos/oreo-morango.png",
+            "name": "Bolo Oreo e Morango",
+            "dsc": "Bolo Oreo Morango",
+            "price": 80
+        }
+    ],
+    "donuts": [
+        {
+            "id": "donuts-brigadeiro",
+            "img": "./img/donuts/donuts-brigadeiro.png",
+            "name": "Donuts Brigadeiro",
+            "dsc": "Donuts Brigadeiro",
+            "price": 8
+        },
+        {
+            "id": "donuts-baunilha",
+            "img": "./img/donuts/donuts-baunilha2.png",
+            "name": "Donuts Baunilha",
+            "dsc": "Donuts Baunilha®",
+            "price": 8
+        },
+        {
+            "id": "donuts-dark",
+            "img": "./img/donuts/donuts-dark.png",
+            "name": "Donuts Baunilha",
+            "dsc": "Donuts Baunilha",
+            "price": 9
+        },
+        {
+            "id": "donuts-docedeleite",
+            "img": "./img/donuts/donuts-docedeleite.png",
+            "name": "Donuts Doce de Leite",
+            "dsc": "Donuts Doce de Leite",
+            "price": 8
+        },
+        {
+            "id": "donuts-morango",
+            "img": "./img/donuts/donuts-morango.png",
+            "name": "Donuts Morango",
+            "dsc": "donuts-morango®",
+            "price": 8
+        },
+        {
+            "id": "donuts-kinderbueno",
+            "img": "./img/donuts/donuts-kinderbueno.png",
+            "name": "Donuts Kinder Bueno",
+            "dsc": "Donuts Kinder Bueno",
+            "price": 9
+        },
+        {
+            "id": "donuts-maracuja",
+            "img": "./img/donuts/donuts-maracuja.png",
+            "name": "Donuts Maracuja",
+            "dsc": "Donuts maracuja",
+            "price": 8
+        },
+        {
+            "id": "donuts-coracao",
+            "img": "./img/donuts/donuts-coracao.png",
+            "name": "Donuts Coracao",
+            "dsc": "donuts-coracao",
+            "price": 9
+        },
+        {
+            "id": "donuts-oreo",
+            "img": "./img/donuts/donuts-oreo.png",
+            "name": "Donuts Oreo",
+            "dsc": "donuts-oreo",
+            "price": 9
+        },
+        {
+            "id": "donuts-azul",
+            "img": "./img/donuts/donuts-azul.png",
+            "name": "Donuts Azul",
+            "dsc": "donuts-azul",
+            "price": 8
+        },
+        {
+            "id": "donuts-pistache",
+            "img": "./img/donuts/donuts-pistache.png",
+            "name": "Donuts Pistache",
+            "dsc": "donuts-pistache",
+            "price": 8
+        },
+        {
+            "id": "donuts-simples",
+            "img": "./img/donuts/donuts-simples.png",
+            "name": "Donuts Simples",
+            "dsc": "donuts-simples",
+            "price": 7
+        }
+    ],
+    "sobremesas": [
+        {
+            "id": "15259-german-chocolate-killer-brownie-tin-pack",
+            "img": "./img/cardapio/sobremesas/german-chocolate-killer-brownie-tin-pack.5ebc34160f28767a9d94c4da2e04c4b9.jpg",
+            "name": "Killer Brownie®",
+            "dsc": "German Chocolate Killer Brownie®",
+            "price": 39.99
+        },
+        {
+            "id": "jacques-world-famous-chocolate-chip-cookies",
+            "img": "./img/cardapio/sobremesas/jacques-world-famous-chocolate-chip-cookies-6-pack.2217a14c443602493bba88aa9335319a.jpg",
+            "name": "Jacques Torres Chocolate",
+            "dsc": "Jacques' World Famous Chocolate Chip Cookies - 6 Pack",
+            "price": 39.95
+        },
+        {
+            "id": "luigis-original-cannoli-pie",
+            "img": "./img/cardapio/sobremesas/original-cannoli-pie.4cb5b9ba82f57b69b90765fd9f07aa1a.jpg",
+            "name": "The Cannoli Pie Company",
+            "dsc": "Original Cannoli Pie",
+            "price": 69
+        },
+        {
+            "id": "sea-salted-caramel-swirl-cheesecake",
+            "img": "./img/cardapio/sobremesas/sea-salted-caramel-swirl-cheesecake.e2825335433fb7a272a5d77649a6849e.jpg",
+            "name": "Cotton Blues Cheesecake Company",
+            "dsc": "Sea-Salted Caramel Swirl Cheesecake",
+            "price": 65
+        },
+        {
+            "id": "brooklyn-blackout-cookie-brownie-combo-pack-2-tins",
+            "img": "./img/cardapio/sobremesas/brooklyn-blackout-cookie-brownie-combo-pack-2-tins.d8805325baf6b23b4f01d119dc4531a7.jpg",
+            "name": "Brooklyn Blackout Company",
+            "dsc": "Brooklyn Blackout Cookie + Brownie Combo Pack - 2 Tins",
+            "price": 89
+        },
+        {
+            "id": "best-seller-cupcake-dozen",
+            "img": "./img/cardapio/sobremesas/best-seller-cupcake-dozen.f93b21993f6a2da11c975d45b9b0d08f.jpg",
+            "name": "Crave Cupcakes",
+            "dsc": "Best Seller Cupcake Dozen",
+            "price": 89
+        },
+        {
+            "id": "choose-your-own-ice-cream-donuts-6-pack",
+            "img": "./img/cardapio/sobremesas/choose-your-own-ice-cream-donuts-6-pack.24d0b44765a7c54237fcd7ea9d9d8093.jpg",
+            "name": "Elegant Desserts",
+            "dsc": "Choose Your Own Ice Cream Donuts - 6 Pack",
+            "price": 69
+        },
+        {
+            "id": "17481-jewish-dessert-3-pack",
+            "img": "./img/cardapio/sobremesas/jewish-classics-dessert-pack.7d4b76630f2fe75dcb7bbcf2501b7390.jpg",
+            "name": "Ess-a-Bagel",
+            "dsc": "Jewish Classics Dessert Pack",
+            "price": 89.95
+        },
+        {
+            "id": "dessert-bar-care-package",
+            "img": "./img/cardapio/sobremesas/dessert-bar-care-package.324aa28fe98c9dc67b75aac43376902e.jpg",
+            "name": "Bread and Roses Bakery",
+            "dsc": "Dessert Bar Care Package",
+            "price": 65
+        },
+        {
+            "id": "donut-cookies-12-pack",
+            "img": "./img/cardapio/sobremesas/donut-cookies-12-pack.46f42c61c4a79fd2051a99b2f23e890e.jpg",
+            "name": "Stan's Donuts",
+            "dsc": "Donut Cookies - 12 Pack",
+            "price": 49
+        },
+        {
+            "id": "gulab-jamun-ice-cream-cakes-2-pack",
+            "img": "./img/cardapio/sobremesas/gulab-jamun-ice-cream-cakes-2-pack.c45b4d0750ad22d741f84dc1f26d20e7.jpg",
+            "name": "Malai Ice Cream",
+            "dsc": "Gulab Jamun Ice Cream Cakes - 2 Pack",
+            "price": 79
+        },
+        {
+            "id": "jacques-world-famous-chocolate-chip-cookies-12-pack",
+            "img": "./img/cardapio/sobremesas/jacques-world-famous-chocolate-chip-cookies-12-pack.3b373bdd67cd25084182c21499f675d1.jpg",
+            "name": "Jacques Torres Chocolate",
+            "dsc": "Jacques' World Famous Chocolate Chip Cookies - 12 Pack",
+            "price": 69.95
+        }
+    ],
+    "bebidas": [
+        {
+            "id": "hong-kong-boba-tea-kit-for-6",
+            "img": "./img/cardapio/bebidas/hong-kong-boba-tea-kit-for-6.63841de36d8e5edfafa13023fc303285.jpg",
+            "name": "New Territories",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 59
+        },
+        {
+            "id": "guys-caliente-margaritas-for-12",
+            "img": "./img/cardapio/bebidas/guys-caliente-margaritas-for-12.ca8c6bc06b8f1039549385ffcebc749d.jpg",
+            "name": "Guy Fieri",
+            "dsc": "Guy's Caliente Margaritas for 12",
+            "price": 69
+        },
+        {
+            "id": "woodford-reserve-mint-julep-syrup",
+            "img": "./img/cardapio/bebidas/woodford-reserve-mint-julep-syrup.ef523ac7cbae5f4aba6b058207f490d2.jpg",
+            "name": "Woodford Reserve",
+            "dsc": "Woodford Reserve Mint Julep Syrup",
+            "price": 39
+        },
+        {
+            "id": "new-orleans-hurricane-mix",
+            "img": "./img/cardapio/bebidas/new-orleans-hurricane-mix.4613584fc65cb0787024dd24d2a8f4b3.jpg",
+            "name": "Franco's Hurricane Mix",
+            "dsc": "New Orleans Hurricane Mix",
+            "price": 39
+        },
+        {
+            "id": "margarita-mix",
+            "img": "./img/cardapio/bebidas/margarita-mix.bd48a000d589d3147b14790af3c33fcd.jpg",
+            "name": "Johnny Sanchez",
+            "dsc": "Margarita Mix",
+            "price": 59
+        },
+        {
+            "id": "woodford-reserve-mint-julep-syrup-2-pack",
+            "img": "./img/cardapio/bebidas/woodford-reserve-mint-julep-syrup-2-pack.0ac76063f151988113cbaabd0eaa829f.jpg",
+            "name": "Woodford Reserve",
+            "dsc": "Woodford Reserve Mint Julep Syrup - 2 Pack",
+            "price": 59
+        },
+        {
+            "id": "unicorn-parade-milkshake-kit-for-8",
+            "img": "./img/cardapio/bebidas/unicorn-parade-milkshake-kit-for-2.9052d04c1cf25b29442048bd3e535f21.jpg",
+            "name": "New Territories",
+            "dsc": "Unicorn Parade Milkshake Kit for 8",
+            "price": 109
+        },
+        {
+            "id": "chickpea-chiller-kit-for-6",
+            "img": "./img/cardapio/bebidas/chickpea-chiller-kit-for-6.4310765c71ba524b5462ea9330d32446.jpg",
+            "name": "The Hummus & Pita Co.",
+            "dsc": "Chickpea Chiller Kit for 6",
+            "price": 89
+        },
+        {
+            "id": "15194-old-honey-barn-mint-julep-mixer-200ml",
+            "img": "./img/cardapio/bebidas/old-honey-barn-mint-julep-mixer-200ml.e0b131d6d9b69963706b43fd4334ab74.jpg",
+            "name": "Old Honey Barn Mint Julep",
+            "dsc": "Old Honey Barn Mint Julep Mixer - 200ml Flask",
+            "price": 25
+        },
+        {
+            "id": "kentucky-derby-mint-julep-gift-set",
+            "img": "./img/cardapio/bebidas/kentucky-derby-mint-julep-gift-set.79720eda4e9c8e3fcf9ecb5c79827f2c.jpg",
+            "name": "Woodford Reserve",
+            "dsc": "Kentucky Derby Mint Julep Gift Set",
+            "price": 59.95
+        },
+        {
+            "id": "002-charleston-bloody-mary-mix-weekender-bold-and-spicy",
+            "img": "./img/cardapio/bebidas/weekender-charleston-bloody-mary-mix-bold-and-spicy.c372868c9937e407a299a22001e210e2.jpg",
+            "name": "Charleston Beverage Company",
+            "dsc": "Weekender | Charleston Bloody Mary Mix Bold & Spicy",
+            "price": 39.95
+        },
+        {
+            "id": "nola-cold-brew-concentrate-bag-in-box",
+            "img": "./img/cardapio/bebidas/nola-cold-brew-concentrate-bag-in-box.3df6fde8bd83f29235565984ae8ed22b.jpg",
+            "name": "Grady's Cold Brew",
+            "dsc": "NOLA Cold Brew Concentrate Bag-in-Box",
+            "price": 40
+        }
+    ],
+    "cupcake": [
+        {
+            "id": "cupcake-amora",
+            "img": "./img/cupcake/amora.png",
+            "name": "Cupcake Amora",
+            "dsc": "cupcake-amora",
+            "price": 7
+        },
+        {
+            "id": "cupcake-arco-iris",
+            "img": "./img/cupcake/arco-iris.png",
+            "name": "Cupcake Arco Iris",
+            "dsc": "cupcake-arco-iris",
+            "price": 7
+        },
+        {
+            "id": "cupcake-blueberry",
+            "img": "./img/cupcake/blueberry.png",
+            "name": "Cupcake Blueberry",
+            "dsc": "cupcake-blueberry",
+            "price": 7
+        },
+        {
+            "id": "cupcake-chocolate",
+            "img": "./img/cupcake/chocolate.png",
+            "name": "Cupcake Chocolate",
+            "dsc": "cupcake-chocolate",
+            "price": 7
+        },
+        {
+            "id": "cupcake-doce-de-leite",
+            "img": "./img/cupcake/doce-de-leite.png",
+            "name": "Cupcake Doce de Leite",
+            "dsc": "cupcake-doce-de-leite",
+            "price": 7
+        },
+        {
+            "id": "cupcake-frutas-vermelhas",
+            "img": "./img/cupcake/frutas-vermelhas.png",
+            "name": "Cupcake Frutas Vermelhas",
+            "dsc": "cupcake-frutas-vermelhas",
+            "price": 7
+        },
+        {
+            "id": "cupcake-limao",
+            "img": "./img/cupcake/limao.png",
+            "name": "Cupcake Limão",
+            "dsc": "cupcake-limao",
+            "price": 7
+        },
+        {
+            "id": "cupcake-morango",
+            "img": "./img/cupcake/morango.png",
+            "name": "Cupcake Morango",
+            "dsc": "cupcake-morango",
+            "price": 7
+        },
+        {
+            "id": "cupcake-oreo",
+            "img": "./img/cupcake/oreo.png",
+            "name": "Cupcake Oreo",
+            "dsc": "cupcake-oreo",
+            "price": 7
+        },
+        {
+            "id": "cupcake-pistache",
+            "img": "./img/cupcake/pistache.png",
+            "name": "Cupcake Pistache",
+            "dsc": "cupcake-pistache",
+            "price": 7
+        },
+        {
+            "id": "cupcake-red-velvet",
+            "img": "./img/cupcake/red-velvet.png",
+            "name": "Cupcake Rede Velvet",
+            "dsc": "cupcake-red-velvet",
+            "price": 7
+        },
+        {
+            "id": "cupcake-unicornio",
+            "img": "./img/cupcake/unicornio.png",
+            "name": "Cupcake Unicornio",
+            "dsc": "cupcake-unicornio",
+            "price": 7
+        }
+    ],
+    "steaks": [
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./img/cardapio/steaks/california-reserve-filet-mignon-steaks-gift-box.bf226e317aad85f47897ae7e325f790d.jpg",
+            "name": "Flannery Beef",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 129
+        },
+        {
+            "id": "steaks-and-cakes-date-night-dinner-for-2",
+            "img": "./img/cardapio/steaks/valentines-steaks-and-cakes-dinner-for-2.0c63dab635eed46209455dc33cd25ea8.jpg",
+            "name": "Chesapeake Bay Gourmet",
+            "dsc": "Steaks and Cakes Date Night Dinner for 2",
+            "price": 129
+        },
+        {
+            "id": "Prime-holiday-steak-sampler-for-10-12",
+            "img": "./img/cardapio/steaks/holiday-sampler-9-pack.2de75ca80282ffe9d064eb757ff7a5a1.jpg",
+            "name": "Saltbrick Prime",
+            "dsc": "Chef Matt's Steak Sampler for 10-12",
+            "price": 179
+        },
+        {
+            "id": "bone-in-rib-steak",
+            "img": "./img/cardapio/steaks/bone-in-rib-steak.b13d9d4233035767605f0de9acdce1ab.jpg",
+            "name": "Old Homestead Steakhouse",
+            "dsc": "Bone-in Rib Steak",
+            "price": 159
+        },
+        {
+            "id": "american-wagyu-gold-grade-top-sirloins",
+            "img": "./img/cardapio/steaks/american-wagyu-gold-grade-sirloins.040dbeb8f8e615b91fa7e513e3dc089c.jpg",
+            "name": "Snake River Farms",
+            "dsc": "American Wagyu Gold Grade Top Sirloins",
+            "price": 119
+        },
+        {
+            "id": "2-peter-luger-steak-pack-b",
+            "img": "./img/cardapio/steaks/peter-luger-steak-pack-b.9feb0300e6be2dfecfa314f2006a2183.jpg",
+            "name": "Peter Luger Steak House",
+            "dsc": "Peter Luger Porterhouse Steaks",
+            "price": 215.95
+        },
+        {
+            "id": "ribeye-prime-steak-gift-box",
+            "img": "./img/cardapio/steaks/ribeye-prime-steak-gift-box.e74cb016baabbb2df73861de8150f29c.jpg",
+            "name": "Churchill's Steakhouse",
+            "dsc": "Ribeye Prime Steak Gift Box",
+            "price": 229
+        },
+        {
+            "id": "dry-aged-usda-prime-black-angus-porterhouse-steak-2-pack",
+            "img": "./img/cardapio/steaks/usda-prime-black-angus-filet-mignon-barrel-cut.6ee213799e7d1848763d12edca18e3b1.jpg",
+            "name": "Pat LaFrieda Meats",
+            "dsc": "Dry-Aged USDA Prime Black Angus Porterhouse Steak - 2 Pack",
+            "price": 96.7
+        },
+        {
+            "id": "california-reserve-ribeye-steak",
+            "img": "./img/cardapio/steaks/california-reserve-ribeye-steak-12-oz.d367c5ae72dd9f89e170662104bef4fc.jpg",
+            "name": "Flannery Beef",
+            "dsc": "California Reserve Ribeye Steak - 12 oz",
+            "price": 32
+        },
+        {
+            "id": "dry-aged-boneless-ribeye-steak-dinner-kit-for-4",
+            "img": "./img/cardapio/steaks/dry-aged-boneless-ribeye-steak-dinner-for-4.81c3bdc05fe6bdb2c2214709863120e0.jpg",
+            "name": "Chef Francis Mallmann",
+            "dsc": "Dry-Aged Boneless Ribeye Steak Dinner Kit for 4",
+            "price": 225
+        },
+        {
+            "id": "california-reserve-filet-mignon-steak",
+            "img": "./img/cardapio/steaks/california-reserve-filet-mignon-steak.ff15071964ec8141d30c2ba05fb117e0.jpg",
+            "name": "Flannery Beef",
+            "dsc": "California Reserve Filet Mignon Steak",
+            "price": 22
+        },
+        {
+            "id": "mesquite-smoked-peppered-beef-tenderloin",
+            "img": "./img/cardapio/steaks/mesquite-smoked-peppered-beef-tenderloin.5c314418a1f75c7057eed686e2fad46f.jpg",
+            "name": "Perini Ranch Steakhouse",
+            "dsc": "Mesquite Smoked Peppered Beef Tenderloin",
+            "price": 165
+        }
+    ]
+}
